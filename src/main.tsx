10e68d5554635226b42b1723ff8322c67f91a34e
codespool <4625220+codespool@users.noreply.github.com>
2025-04-02 14:30:59 +0300

    add google login

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import "@rainbow-me/rainbowkit/styles.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider } from "wagmi";
 
-import { RainbowKitProvider, darkTheme } from "@rainbow-me/rainbowkit";
+import { PrivyProvider } from "@privy-io/react-auth";
+import { soneiumMinato } from "viem/chains";
 import App from "./App.tsx";
 import { config } from "./config.ts";
 
@@ -14,12 +14,28 @@ const queryClient = new QueryClient();
 
 createRoot(root).render(
   <StrictMode>
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider theme={darkTheme()} modalSize="compact">
+    <PrivyProvider
+      appId="cm8pwpq5r021np8n16w4njn7e"
+      config={{
+        // Display email and wallet as login methods
+        loginMethods: ["email", "google", "wallet"],
+        appearance: {
+          theme: "light",
+          accentColor: "#676FFF",
+        },
+        // Create embedded wallets for users who don't have a wallet
+        embeddedWallets: {
+          createOnLogin: "all-users",
+        },
+        supportedChains: [soneiumMinato],
+        defaultChain: soneiumMinato,
+      }}
+    >
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
           <App />
-        </RainbowKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
+        </QueryClientProvider>
+      </WagmiProvider>
+    </PrivyProvider>
   </StrictMode>,
 );
