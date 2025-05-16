ec4b52f18e69ff86d3290d88921119ff98370e1f
livingrockrises <90545960+livingrockrises@users.noreply.github.com>
2025-05-16 16:59:47 +0400

    Updates

---
+++ assets/cursor-DAQzUpC-.js
@@ -0,0 +1,3 @@
+import{D as o}from"./index-BIPrX1Fj.js";import"./index-Cpm0qesE.js";const i=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{i as cursorSvg};
